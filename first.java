2a7bbe235469f930ad491c28b67181bbc307d14d
Hithendra B M <vvce21cse0004@vvce.ac.in>
2024-07-10 15:15:11 +0530

    Update first.java

--- first.java
+++ first.java
@@ -1,18 +1,14 @@
-import java.util.Scanner;
+// Java code to illustrate 
+// System.out.println(); 
+import java.io.*; 
 
-class HelloWorld {
-
-    public static void main(String[] args) {
-
-        // Creates a reader instance which takes
-        // input from standard input - keyboard
-        Scanner reader = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-
-        // nextInt() reads the next integer from the keyboard
-        int number = reader.nextInt();
-
-        // println() prints the following line to the output screen
-        System.out.println("You entered: " + number);
-    }
-}
+// Driver Class 
+class first{ 
+	// main function 
+	public static void main(String[] args) 
+	{ 
+		System.out.println("Welcome"); 
+		System.out.println("To"); 
+		System.out.println("GeeksforGeeks"); 
+	} 
+} 
